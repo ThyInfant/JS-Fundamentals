6a15f6f67d6190d2055843d02b3340e67d302924
Rob Tade <robtade4@gmail.com>
2025-11-08 02:57:17 +0300

    Add 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const arg = process.argv[2];
+const times = parseInt(arg);
+
+if (Number.isNaN(times) || times <= 0) {
+  console.log("Missing number of occurrences");
+} else {
+  let i = 0;
+  while (i < times) {
+    console.log("C is fun");
+    i++;
+  }
+}

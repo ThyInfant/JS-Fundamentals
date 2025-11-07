ab063bf21599981dc7881d1539c20c47bfc18286
Robsan <robtade4@gmail.com>
2025-11-08 01:21:51 +0300

    Delete 2-value_argument.js

--- 2-value_argument.js
+++
@@ -1,9 +0,0 @@
-const args = process.argv.slice(2);
-
-if (args[0] === undefined) {
-  console.log("No argument");
-} else if (args[1] === undefined) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
